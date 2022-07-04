2cffc9c04bd5535a58107b60d3f04cfdb9d4b8d1
yui666a <haiso666@gmail.com>
2022-07-04 15:49:38 +0900

    reset js

--- script.js
+++ script.js
@@ -1,50 +1,46 @@
-let enemy = document.querySelector("#sofaObj");
-enemy.addEventListener("click", function () {
-  console.log("sofaObj クリックと");
+
+var video = document.getElementById("bird");
+var music_on = document.getElementById("music_on");
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  var scene = document.querySelector("a-scene");
+
+  if (scene.hasLoaded) {
+    run();
+  } else {
+    scene.addEventListener("loaded", run);
+  }
+
+  function run() {
+    scene.querySelector("#sofaObj").addEventListener("click", function (e) {
+      // video.play();
+      console.log("sofaObj クリックと")
+    });
+    // scene.querySelector("#sofaObj2").addEventListener("click", function (e) {
+    //   console.log("sofaObj2 クリックと")
+    // });
+  }
 });
 
-// var video = document.getElementById("bird");
-// var music_on = document.getElementById("music_on");
+AFRAME.registerComponent("click-stop", {
+  init: function () {
+    this.el.addEventListener("click", function (event) {
+      video.pause();
+    });
+  },
+});
 
-// document.addEventListener("DOMContentLoaded", function (event) {
-//   var scene = document.querySelector("a-scene");
-
-//   if (scene.hasLoaded) {
-//     run();
-//   } else {
-//     scene.addEventListener("loaded", run);
-//   }
-
-//   function run() {
-//     scene.querySelector("#sofaObj").addEventListener("click", function (e) {
-//       // video.play();
-//       console.log("sofaObj クリックと")
-//     });
-//     // scene.querySelector("#sofaObj2").addEventListener("click", function (e) {
-//     //   console.log("sofaObj2 クリックと")
-//     // });
-//   }
-// });
-
-// AFRAME.registerComponent("click-stop", {
-//   init: function () {
-//     this.el.addEventListener("click", function (event) {
-//       video.pause();
-//     });
-//   },
-// });
-
-// AFRAME.registerComponent("click-music", {
-//   init: function () {
-//     this.el.addEventListener("click", function (event) {
-//       if (video.muted) {
-//         //video.muted = !video.muted; //this also works
-//         video.muted = false;
-//         this.setAttribute("material", "src", music_on);
-//       } else {
-//         video.muted = true;
-//         this.setAttribute("material", "src", "#music_off");
-//       }
-//     });
-//   },
-// });
+AFRAME.registerComponent("click-music", {
+  init: function () {
+    this.el.addEventListener("click", function (event) {
+      if (video.muted) {
+        //video.muted = !video.muted; //this also works
+        video.muted = false;
+        this.setAttribute("material", "src", music_on);
+      } else {
+        video.muted = true;
+        this.setAttribute("material", "src", "#music_off");
+      }
+    });
+  },
+});
