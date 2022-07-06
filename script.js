b56d8fbd123ee81a5b40fb5c3e321072a2c1a358
yui666a <haiso666@gmail.com>
2022-07-06 16:45:17 +0900

    input listener

--- script.js
+++ script.js
@@ -1,46 +1,64 @@
+function inputChange(event){
+  console.log('お名前は' + xPositionText.value + 'さんですね');
+}
 
-var video = document.getElementById("bird");
-var music_on = document.getElementById("music_on");
+let xPositionText = document.getElementById('xPositionText');
+xPositionText.addEventListener('change', inputChange);
+// let msg = document.getElementById('msg');
 
-document.addEventListener("DOMContentLoaded", function (event) {
-  var scene = document.querySelector("a-scene");
 
-  if (scene.hasLoaded) {
-    run();
-  } else {
-    scene.addEventListener("loaded", run);
-  }
+AFRAME.registerComponent('collider-check', {
+  dependencies: ['raycaster'],
 
-  function run() {
-    scene.querySelector("#sofaObj").addEventListener("click", function (e) {
-      // video.play();
-      console.log("sofaObj クリックと")
+  init: function () {
+    this.el.addEventListener('raycaster-intersection', function () {
+      console.log('Player hit something!');
     });
-    // scene.querySelector("#sofaObj2").addEventListener("click", function (e) {
-    //   console.log("sofaObj2 クリックと")
-    // });
   }
 });
 
-AFRAME.registerComponent("click-stop", {
-  init: function () {
-    this.el.addEventListener("click", function (event) {
-      video.pause();
-    });
-  },
-});
+// var video = document.getElementById("bird");
+// var music_on = document.getElementById("music_on");
 
-AFRAME.registerComponent("click-music", {
-  init: function () {
-    this.el.addEventListener("click", function (event) {
-      if (video.muted) {
-        //video.muted = !video.muted; //this also works
-        video.muted = false;
-        this.setAttribute("material", "src", music_on);
-      } else {
-        video.muted = true;
-        this.setAttribute("material", "src", "#music_off");
-      }
-    });
-  },
-});
+// document.addEventListener("DOMContentLoaded", function (event) {
+//   var scene = document.querySelector("a-scene");
+
+//   if (scene.hasLoaded) {
+//     run();
+//   } else {
+//     scene.addEventListener("loaded", run);
+//   }
+
+//   function run() {
+//     scene.querySelector("#sofaObj").addEventListener("click", function (e) {
+//       // video.play();
+//       console.log("sofaObj クリックと")
+//     });
+//     // scene.querySelector("#sofaObj2").addEventListener("click", function (e) {
+//     //   console.log("sofaObj2 クリックと")
+//     // });
+//   }
+// });
+
+// AFRAME.registerComponent("click-stop", {
+//   init: function () {
+//     this.el.addEventListener("click", function (event) {
+//       video.pause();
+//     });
+//   },
+// });
+
+// AFRAME.registerComponent("click-music", {
+//   init: function () {
+//     this.el.addEventListener("click", function (event) {
+//       if (video.muted) {
+//         //video.muted = !video.muted; //this also works
+//         video.muted = false;
+//         this.setAttribute("material", "src", music_on);
+//       } else {
+//         video.muted = true;
+//         this.setAttribute("material", "src", "#music_off");
+//       }
+//     });
+//   },
+// });
