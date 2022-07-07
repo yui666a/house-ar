901d6b8d08797bddc9555165d71b496b83aab7df
yui666a <haiso666@gmail.com>
2022-07-07 22:07:24 +0900

    duplicate script.js

---
+++ script2.js
@@ -0,0 +1,66 @@
+function inputChange(event){
+  console.log('xPosition: ' + xPositionText.value );
+}
+function upClicked(){
+  console.log('xPosition: ' + xPositionText.value );
+}
+
+let xPositionText = document.getElementById('xPositionText');
+xPositionText.addEventListener('change', inputChange);
+
+
+AFRAME.registerComponent('collider-check', {
+  dependencies: ['raycaster'],
+
+  init: function () {
+    this.el.addEventListener('raycaster-intersection', function () {
+      console.log('Player hit something!');
+    });
+  }
+});
+
+// var video = document.getElementById("bird");
+// var music_on = document.getElementById("music_on");
+
+// document.addEventListener("DOMContentLoaded", function (event) {
+//   var scene = document.querySelector("a-scene");
+
+//   if (scene.hasLoaded) {
+//     run();
+//   } else {
+//     scene.addEventListener("loaded", run);
+//   }
+
+//   function run() {
+//     scene.querySelector("#sofaObj").addEventListener("click", function (e) {
+//       // video.play();
+//       console.log("sofaObj クリックと")
+//     });
+//     // scene.querySelector("#sofaObj2").addEventListener("click", function (e) {
+//     //   console.log("sofaObj2 クリックと")
+//     // });
+//   }
+// });
+
+// AFRAME.registerComponent("click-stop", {
+//   init: function () {
+//     this.el.addEventListener("click", function (event) {
+//       video.pause();
+//     });
+//   },
+// });
+
+// AFRAME.registerComponent("click-music", {
+//   init: function () {
+//     this.el.addEventListener("click", function (event) {
+//       if (video.muted) {
+//         //video.muted = !video.muted; //this also works
+//         video.muted = false;
+//         this.setAttribute("material", "src", music_on);
+//       } else {
+//         video.muted = true;
+//         this.setAttribute("material", "src", "#music_off");
+//       }
+//     });
+//   },
+// });
